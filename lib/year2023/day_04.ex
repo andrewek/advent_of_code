ea8a25cb39accabe994408fa1c10619c7df17103
Andrew Ek <andrewek@gmail.com>
2023-12-10 10:35:13 -0600

    2023-Day4 - Part 2

--- lib/year2023/day_04.ex
+++ lib/year2023/day_04.ex
@@ -24,7 +24,50 @@ defmodule AdventOfCode.Year2023.Day04 do
   end
 
   def calc_part_2(inputs) do
-    inputs
+    parsed_inputs =
+      inputs
+      |> Enum.map(&parse_line/1)
+
+    counter = build_counter(length(parsed_inputs))
+
+    final_counter = process_inputs_pt_2(parsed_inputs, counter)
+    IO.inspect(final_counter, label: "Final Counter")
+
+    total_card_count(final_counter)
+  end
+
+  def process_inputs_pt_2([], counter) do
+    counter
+  end
+
+  def process_inputs_pt_2([current_card | remaining_cards], counter) do
+    %{
+      card_number: current_index,
+      match_count: match_count
+    } = current_card
+
+    new_counter = increment_count(counter, current_index, match_count)
+
+    process_inputs_pt_2(remaining_cards, new_counter)
+  end
+
+  # If no matches, we add no cards
+  def increment_count(counter, _, 0) do
+    counter
+  end
+
+  def increment_count(counter, current_index, match_count) do
+    card_count = counter[current_index]
+
+    # If currently card 3 with 4 matches, we add to cards 4, 5, 6, and 7
+    indices_to_increment = (current_index + 1)..(current_index + match_count)
+
+    indices_to_increment
+    |> Enum.reduce(counter, fn(idx, accumulator) ->
+      current_count = accumulator[idx]
+      new_count = current_count + card_count
+      Map.put(accumulator, idx, new_count)
+    end)
   end
 
   def parse_and_score_line(line) do
@@ -41,18 +84,6 @@ defmodule AdventOfCode.Year2023.Day04 do
     |> put_ticket_numbers()
     |> put_intersection()
     |> put_score()
-  end
-
-  def score(%{match_count: 0}) do
-    0
-  end
-
-  def score(%{match_count: count}) do
-    2 ** (count - 1)
-  end
-
-  defp put_score(input) do
-    Map.put(input, :score, score(input))
   end
 
   defp put_card_number(%{raw_input: line} = input) do
@@ -82,6 +113,10 @@ defmodule AdventOfCode.Year2023.Day04 do
     Map.put(input, :ticket_numbers, ticket_numbers)
   end
 
+  defp put_score(input) do
+    Map.put(input, :score, score(input))
+  end
+
   defp put_winning_numbers(%{raw_input: line} = input) do
     [_, win_str] = Regex.run(~r/:\s*(.+)\s\|/, line, trim: true)
 
@@ -92,5 +127,24 @@ defmodule AdventOfCode.Year2023.Day04 do
       |> MapSet.new()
 
     Map.put(input, :winning_numbers, winning_numbers)
+  end
+
+  def score(%{match_count: 0}) do
+    0
+  end
+
+  def score(%{match_count: count}) do
+    2 ** (count - 1)
+  end
+
+  def build_counter(count) when count >= 1 do
+    Enum.reduce(1..count, %{}, fn(idx, accumulator) ->
+      Map.put(accumulator, idx, 1)
+    end)
+  end
+
+  def total_card_count(counter) do
+    counter
+    |> Enum.reduce(0, fn({_, count}, sum) -> sum + count end)
   end
 end
--- test/year2023/day_04_test.exs
+++ test/year2023/day_04_test.exs
@@ -31,4 +31,99 @@ defmodule AdventOfCode.Year2023.Day04Test do
       } == Subject.parse_line(input)
     end
   end
+
+  describe "calc_part_2/1" do
+    test "it works" do
+      inputs = [
+        "Card 1: 41 48 83 86 17 | 83 86  6 31 17  9 48 53",
+        "Card 2: 13 32 20 16 61 | 61 30 68 82 17 32 24 19",
+        "Card 3:  1 21 53 59 44 | 69 82 63 72 16 21 14  1",
+        "Card 4: 41 92 73 84 69 | 59 84 76 51 58  5 54 83",
+        "Card 5: 87 83 26 28 32 | 88 30 70 12 93 22 82 36",
+        "Card 6: 31 18 13 56 72 | 74 77 10 23 35 67 36 11"
+      ]
+
+      assert 30 == Subject.calc_part_2(inputs)
+    end
+  end
+
+  describe "increment_counter/" do
+    test "handles no matches" do
+      input = %{
+        1 => 1,
+        2 => 1,
+        3 => 1,
+        4 => 1
+      }
+
+      assert input == Subject.increment_count(input, 1, 0)
+      assert input == Subject.increment_count(input, 3, 0)
+    end
+
+    test "handles 1 match and 1 card" do
+      input = %{
+        1 => 1,
+        2 => 1,
+        3 => 1,
+        4 => 1
+      }
+
+      # We have one of card one, and one match; now we need one extra of card 2
+      assert %{input | 2 => 2} == Subject.increment_count(input, 1, 1)
+
+      # We have one of card 3 and 1 match. Now we need 1 extra of card 4
+      assert %{input | 4 => 2} == Subject.increment_count(input, 3, 1)
+    end
+
+    test "handles many matches and 1 card" do
+      input = %{
+        1 => 1,
+        2 => 1,
+        3 => 1,
+        4 => 1
+      }
+
+      # 3 matches, so 1 extra of cards 2-4
+      assert %{input | 2 => 2, 3 => 2, 4 => 2} == Subject.increment_count(input, 1, 3)
+    end
+
+    test "handles many cards and 1 match" do
+      input = %{
+        1 => 3,
+        2 => 1,
+        3 => 1,
+        4 => 1
+      }
+
+      # 1 match on 3 cards, so 3 extra of card 2
+      assert %{input | 2 => 4} == Subject.increment_count(input, 1, 1)
+    end
+
+    test "handles many matches and many cards" do
+      input = %{
+        1 => 3,
+        2 => 1,
+        3 => 1,
+        4 => 1
+      }
+
+      # 3 matches on 3 cards, so 3 extra of cards 2-4
+      assert %{input | 2 => 4, 3 => 4, 4 => 4} == Subject.increment_count(input, 1, 3)
+    end
+  end
+
+  describe "build_counter/1" do
+    test "it populates one item" do
+      assert %{1 => 1} == Subject.build_counter(1)
+    end
+
+    test "it populates a bunch" do
+      assert %{
+        1 => 1,
+        2 => 1,
+        3 => 1,
+        4 => 1
+      } == Subject.build_counter(4)
+    end
+  end
 end
